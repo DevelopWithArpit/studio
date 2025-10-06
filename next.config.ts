5e6afe6f62660f84aea7bdbb28faa7ae5767b07c
Arpit Pise <arpitpise1@gmail.com>
2025-10-06 16:57:57 +0000

    Try fixing this error: `Runtime Error: Error: Failed to fetch. Error sou

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   serverActions: {
-    maxDuration: 120, // 2 minutes
+    maxDuration: 300, // 5 minutes
   },
   typescript: {
     ignoreBuildErrors: true,
